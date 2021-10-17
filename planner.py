98d20cfaad1e6ffe94e0146bf96eb6c6ea2c9d1a
Farshid Asadi <farshidasadi47@yahoo.com>
2021-10-16 22:07:13 -0500

    test folder is added to .gitignore

--- planner.py
+++ planner.py
@@ -3,3 +3,16 @@
 # system.
 # Author: Farshid Asadi, farshidasadi47@yahoo.com
 ########## Libraries ###################################################
+import casadi
+
+########## classes and functions #######################################
+nx = 1
+nz = 1
+z = casadi.SX.sym('x',nz)
+x = casadi.SX.sym('x',nx)
+g0 = casadi.sin(x+z)
+g1 = casadi.cos(x-z)
+g = casadi.Function('g',[z,x],[g0,g1])
+G = casadi.rootfinder('G','newton',g)
+print(G)
+casadi.disp(G)
