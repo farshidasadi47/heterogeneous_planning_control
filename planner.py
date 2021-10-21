6a8ca2d6a724a40d673eef1ac03425e1ae776ae4
Farshid Asadi <farshidasadi47@yahoo.com>
2021-10-20 20:48:16 -0500

    Planner initialization and var construction added.
    Some initialization methods and constructing symbolic optimization
    variable is added.
    
    python gitignore is added.

--- planner.py
+++ planner.py
@@ -3,16 +3,100 @@
 # system.
 # Author: Farshid Asadi, farshidasadi47@yahoo.com
 ########## Libraries ###################################################
-import casadi
+from itertools import combinations
+from collections import deque
 
+import numpy as np
+import casadi as ca
+
+import model
 ########## classes and functions #######################################
-nx = 1
-nz = 1
-z = casadi.SX.sym('x',nz)
-x = casadi.SX.sym('x',nx)
-g0 = casadi.sin(x+z)
-g1 = casadi.cos(x-z)
-g = casadi.Function('g',[z,x],[g0,g1])
-G = casadi.rootfinder('G','newton',g)
-print(G)
-casadi.disp(G)
+class Planner():
+    """This class contains objects and methods for planning a path
+    for a given swarm of milirobots."""
+
+    def __init__(self, swarm: model.Swarm, n_inner = 2, n_outer = 2):
+        self.swarm = swarm
+        self.robot_combinations = self.__set_robot_combinations()
+        self.mode_sequence = self.__set_mode_sequence()
+        self.x_final = None
+        self.n_inner = n_inner
+        self.n_outer = n_outer
+        self.ub_space_x = 105
+        self.lb_space_x = -105
+        self.ub_space_y = 85
+        self.lb_space_y = -85
+        self.X, self.U = self.__construct_vars()
+        self.P = None
+    
+    def set_space_limit(self, ubx, lbx, uby, lby):
+        """Sets space boundary limits and updates the bands in
+        optimization problem."""
+        self.ub_space_x = ubx
+        self.lb_space_x = lbx
+        self.ub_space_y = uby
+        self.lb_space_y = lby
+    
+    def __set_robot_combinations(self):
+        """Gives possible robot combinations for constraints
+        construction."""
+        robot_combinations = combinations(range(self.swarm.specs.n_robot),2)
+        return list(robot_combinations)
+    
+    def __set_mode_sequence(self):
+        """Gives the current mode sequence."""
+        current_mode = self.swarm.mode
+        mode_sequence = deque(range(1,self.swarm.specs.n_mode))
+        mode_sequence.rotate(-current_mode+1)
+        mode_sequence.appendleft(0)
+        return list(mode_sequence)
+    
+    def __construct_vars(self):
+        """Constructing casadi symbolic variables for optimization."""
+        n_inner = self.n_inner
+        n_outer = self.n_outer
+        n_mode = self.swarm.specs.n_mode
+        n_robot = self.swarm.specs.n_robot
+        X = ca.SX.sym('x',2*n_robot,n_outer*(n_inner+1)*(n_mode-1))
+        U = ca.SX.sym('u',2,n_outer*(n_inner+1)*(n_mode-1))
+        counter = 0
+        for i_outer in range(n_outer):
+            for mode in range(1,n_mode):
+                for i_inner in range(n_inner+1):
+                    varstr ='{:02d}_{:02d}_{:02d}'.format(i_outer,mode,i_inner)
+                    for robot in range(n_robot):
+                        rob_str = '_{:02d}'.format(robot)
+                        X[2*robot,counter] = ca.SX.sym('x_'+varstr+rob_str)
+                        X[2*robot + 1,counter] = ca.SX.sym('y_'+varstr+rob_str)
+                    if i_inner == n_inner:
+                    # if this is last steps of this mode use rotation
+                        i_inner = 0
+                        mode = 0
+                        varstr ='{:02d}_{:02d}_{:02d}'.format(i_outer,
+                                                              mode,i_inner)
+                    U[0,counter] = ca.SX.sym('r_'+varstr+rob_str)
+                    U[1,counter] = ca.SX.sym('t_'+varstr+rob_str)
+                    counter += 1
+        return X, U
+
+    def set_constraint(self):
+        pass
+
+########## test section ################################################
+if __name__ == '__main__':
+    swarm_specs = model.SwarmSpecs(np.array([[9,7,5,3],[3,5,7,9],[3,2,6,9]]),
+                                   5, 10)
+    swarm = model.Swarm(np.array([0,0,10,0,20,0,30,0]), 0, 1, swarm_specs)
+    planner = Planner(swarm)
+
+    #print(planner.swarm.position)
+    #print(planner.swarm.specs.B)
+    #print(planner.swarm.specs.beta)
+    #print(planner.robot_combinations)
+    #print(planner.mode_sequence)
+    print(planner.X.T)
+    print(planner.U.T)
+"""     print(planner.swarm.specs.n_robot)
+    print(planner.swarm.specs.n_mode)
+    print(planner.n_inner)
+    print(planner.n_outer) """
