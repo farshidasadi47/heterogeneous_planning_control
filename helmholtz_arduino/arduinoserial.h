36291ff2f4e6dd01fb36326b92b128bf4b7d30ae
Farshid Asadi <farshidasadi47@yahoo.com>
2021-12-16 14:27:44 -0600

    "arduinoserial.h" is added to the project.

---
+++ helmholtz_arduino/arduinoserial.h
@@ -0,0 +1,24 @@
+/*
+  This header file contains class and methods for our serial
+  communication protocol.
+  The protocols should match those in "arduinoserial.py" module.
+  
+  Author: Farshid Asadi, farshidasadi47@yahoo.com
+  Decemer 2021.
+*/
+#ifndef ARDUINOSERIAL_H
+#define ARDUINOSERIAL_H
+
+#include "Arduino.h"
+/********* Type defs **************************************************/
+
+/********* Function declarations **************************************/
+// some_func
+// Description.
+// Input: None
+// Output: None
+void some_func();
+
+
+
+#endif // ARDUINOSERIAL_H
