c945f4f0291e9263e116168a790deb4c59f74a9c
Farshid Asadi <farshidasadi47@yahoo.com>
2022-01-24 16:56:15 -0600

    git folder is move to include ROS files.
    git repo is moved to its parent folder to include ROS related files
    of the package.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from setuptools import find_packages
+
+package_name = 'swarm'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='farshid asadi',
+    maintainer_email='farshidasadi47@yahoo.com',
+    description='Swarm of magnetic milli-robot planning and control',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = swarm.test:main',
+        ],
+    },
+)
