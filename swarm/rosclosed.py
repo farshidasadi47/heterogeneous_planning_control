b67b19103a14fec5020822ce35925cd369003c8a
Farshid Asadi <farshidasadi47@yahoo.com>
2022-06-21 23:07:09 -0500

    W: cart_pivot action server added to rosclosed for closedloop pivotwalk.
    This accepts cartesian coordinates goal.

--- swarm/rosclosed.py
+++ swarm/rosclosed.py
@@ -238,6 +238,9 @@ class ControlNode(NodeTemplate):
                                                 self._mode_change_server_fb)
         self._add_action_server(RotateAbsolute,'/tumbling',
                                                 self._tumbling_server_fb)
+        # Closed loops
+        self._add_action_server(RotateAbsolute,'cart_pivot',
+                                                  self._cart_pivot_server_cb)
     
     def _arduino_field_fb_cb(self, msg):
         """Call back for /arduino_field_fb."""
@@ -567,6 +570,88 @@ class ControlNode(NodeTemplate):
         else:
             print("Not in idle mode. Current server call is ignored.")
         print("*"*72)
+        # Neutral magnetic field.
+        goal_handle.succeed()
+        result = RotateAbsolute.Result()
+        self.publish_field([0.0]*3)
+        self.cmd_mode = "idle"
+        self.rate.sleep()
+        return result
+
+    def _cart_pivot_server_cb(self,goal_handle):
+        """Executes closed loop pivot alking for one robot."""
+        cnt = 0
+        print("*"*72)
+        regex = r'([+-]?\d+\.?\d*(, *| +)){1}([+-]?\d+\.?\d* *)'
+        field = [0.0,0.0,0.0]
+        self.rate.sleep()
+        if self.cmd_mode == "idle":
+            self.cmd_mode == "busy"
+            try:
+                print("Enter final position: [xf, yf].")
+                print("Enter \"q\" for quitting.")
+                # Read user input.
+                in_str = input("Enter values: ").strip()
+                # Check if user requests quitting.
+                if re.match('q',in_str) is not None:
+                    print("Exitted \"set_idle\".")
+                    raise ValueError
+                # Check if user input matches the template.
+                if re.fullmatch(regex,in_str) is None:
+                    print("Invalid input. Exitted service request.")
+                    raise ValueError
+                # Parse user input.
+                xf =list(map(float,re.findall(r'[+-]?\d+\.?\d*',in_str)))
+                xf *= self.control.specs.n_robot
+                # Get current position
+                _, feedback = self.get_subs_values()
+                xi, theta_i = self.control.process_robots(feedback)
+                msg_i = f"xi: [" + ",".join(f"{i:+07.2f}" for i in xi) + "]"
+                msg_i += f", theta_i: {np.rad2deg(theta_i):+07.2f}"
+                print(msg_i)
+                msg_f = f"xf: [" + ",".join(f"{i:+07.2f}" for i in xf) + "]"
+                print(msg_f)
+                # Reset state
+                self.control.reset_state(pos = xi, theta = theta_i)
+                self.publish_field(field)
+                self.rate.sleep()
+                # Execute closed loop control.
+                iterator = self.control.closed_pivot_cart(xf)
+                self.rate.sleep()
+                for field in iterator:
+                    # Get current position
+                    field_fb, feedback = self.get_subs_values()
+                    state_fb = self.control.process_robots(feedback)
+                    if field is None:
+                        field = iterator.send(state_fb)
+                    field.append(self.control.power)
+                    state = self.control.get_state()[:4]
+                    self.print_stats(field, field_fb, state, state_fb,cnt)
+                    self.publish_field(field)
+                    cnt += 1
+                    self.rate.sleep()
+                # Get current position
+                field_fb, feedback = self.get_subs_values()
+                state_fb = self.control.process_robots(feedback)
+                self.print_stats(field, field_fb, state, state_fb,cnt)
+                self.rate.sleep()
+                _, feedback = self.get_subs_values()
+                xf, theta_f = self.control.process_robots(feedback)
+                msg = f"xf: [" + ",".join(f"{i:+07.2f}" for i in xf) + "]"
+                msg += f", theta_f: {np.rad2deg(theta_f):+07.2f}"
+                print(msg_i)
+                print(msg_f)
+                print(msg)
+                polar_dist = self.control.get_polar_cmd(xi,xf)
+                print(f"r:{polar_dist[0]:+07.2f},phi: {polar_dist[1]:+07.2f}")
+                self.publish_field([0.0]*3)
+                self.rate.sleep()
+            except StopIteration: pass
+            except Exception as exc:
+                print(type(exc).__name__,exc.args)
+                pass
+        else:
+            print("Not in idle mode. Current server call is ignored.")
         # Neutral magnetic field.
         goal_handle.succeed()
         result = RotateAbsolute.Result()
